3f13844765129459c9a1442e1df9faf839cae02c
Sabina <sabinal@nmbu.no>
2019-11-18 14:08:12 +0100

    Started to make new tests

--- src/pa02/test_pa02.py
+++ src/pa02/test_pa02.py
@@ -3,5 +3,39 @@
 __author__ = 'Johan Stabekk, Sabina Langås'
 __email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
 
-import chutes_simulation as ss
+import chutes_simulation as cs
 import pytest
+
+
+def test_resilient_player_move():
+
+    board = cs.Board()
+    player = cs.ResilientPlayer(board)
+    for _ in range(10):
+        player.move()
+
+    assert player.number_of_moves == 10
+
+
+def test_resilient_player_position():
+
+    board = cs.Board()
+    player = cs.ResilientPlayer(board)
+    start_pos = player.position
+    player.move()
+    end_pos = player.position
+
+    assert start_pos is not end_pos
+
+
+def test_resilient_player_position_not_equal_one():
+
+    board = cs.Board()
+    player = cs.ResilientPlayer(board)
+
+    for _ in range(50):
+        player.move()
+        assert 1 < player.position
+
+
+
