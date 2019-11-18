9b085a135ad54b19b7ddafd414cad7fa79777f4c
Johan Stabekk <johan.stabekk@nmbu.no>
2019-11-18 14:52:55 +0100

    Added some more tests to check that the different players works

--- src/pa02/test_pa02.py
+++ src/pa02/test_pa02.py
@@ -1,41 +1,124 @@
 # -*- coding: utf-8 -*-
 
-__author__ = 'Johan Stabekk, Sabina Langås'
-__email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
-
 import chutes_simulation as cs
 import pytest
 
+__author__ = 'Johan Stabekk, Sabina Langås'
+__email__ = 'johansta@nmbu.no, sabinal@nmbu.no'
 
-def test_resilient_player_move():
 
-    board = cs.Board()
-    player = cs.ResilientPlayer(board)
-    for _ in range(10):
+class TestPlayer:
+    def test_move(self):
+
+        board = cs.Board()
+        player = cs.Player(board)
+        for _ in range(10):
+            player.move()
+
+        assert player.number_of_moves == 10
+
+    def test_position(self):
+
+        board = cs.Board()
+        player = cs.Player(board)
+        start_pos = player.position
         player.move()
+        end_pos = player.position
 
-    assert player.number_of_moves == 10
+        assert start_pos is not end_pos
+
+    def test_position_not_equal_one(self):
+
+        board = cs.Board()
+        player = cs.Player(board)
+
+        for _ in range(50):
+            player.move()
+            assert 1 <= player.position
+
+    def test_start_of_chute(self):
+
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+
+        for step in range(50):
+            player.move()
+            assert player.position not in board.snakes_and_ladders.keys()
 
 
-def test_resilient_player_position():
+class TestLazyPlayer:
+    def test_move(self):
 
-    board = cs.Board()
-    player = cs.ResilientPlayer(board)
-    start_pos = player.position
-    player.move()
-    end_pos = player.position
+        board = cs.Board()
+        player = cs.LazyPlayer(board)
+        for _ in range(10):
+            player.move()
 
-    assert start_pos is not end_pos
+        assert player.number_of_moves == 10
 
+    def test_position(self):
 
-def test_resilient_player_position_not_equal_one():
-
-    board = cs.Board()
-    player = cs.ResilientPlayer(board)
-
-    for _ in range(50):
+        board = cs.Board()
+        player = cs.LazyPlayer(board)
+        start_pos = player.position
         player.move()
-        assert 1 < player.position
+        end_pos = player.position
+
+        assert start_pos is not end_pos
+
+    def test_position_not_equal_one(self):
+
+        board = cs.Board()
+        player = cs.LazyPlayer(board)
+
+        for _ in range(50):
+            player.move()
+            assert 1 <= player.position
+
+    def test_start_of_chute(self):
+
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+
+        for step in range(50):
+            player.move()
+            assert player.position not in board.snakes_and_ladders.keys()
 
 
+class TestResilientPlayer:
+    def test_move(self):
 
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+        for _ in range(10):
+            player.move()
+
+        assert player.number_of_moves == 10
+
+    def test_position(self):
+
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+        start_pos = player.position
+        player.move()
+        end_pos = player.position
+
+        assert start_pos is not end_pos
+
+    def test_position_not_equal_one(self):
+
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+
+        for _ in range(50):
+            player.move()
+            assert 1 <= player.position
+
+    def test_start_of_chute(self):
+
+        board = cs.Board()
+        player = cs.ResilientPlayer(board)
+
+        for step in range(50):
+            player.move()
+            assert player.position not in board.snakes_and_ladders.keys()
